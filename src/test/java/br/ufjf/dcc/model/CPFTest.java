106da98209deb08ed07588cc63826b45db884692
gleiph <gleiph@ice.ufjf.br>
2025-03-23 19:46:23 -0300

    test: refactor CPFTest for better readability and assertion messages

--- src/test/java/br/ufjf/dcc/model/CPFTest.java
+++ src/test/java/br/ufjf/dcc/model/CPFTest.java
@@ -3,36 +3,25 @@ package br.ufjf.dcc.model;
 import br.ufjf.dcc.model.exception.InvalidCPFException;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CPFTest {
 
     @Test
-    public void isInvalidCPFFormat() {
-        boolean actual = catchInvalidCPFException("123.456.67-37").isPresent();
-        assertTrue(actual);
+    public void shouldThrowExceptionForInvalidFormat() {
+        assertThrows(InvalidCPFException.class, () -> CPF.getInstance("123.456.67-37"),
+                "Expected InvalidCPFException for incorrectly formatted CPF.");
     }
 
     @Test
-    public void isValidCPFFormatButInvalid() {
-        boolean actual = catchInvalidCPFException("116.008.450-52").isPresent();
-        assertTrue(actual);
+    public void shouldThrowExceptionForInvalidCPFDespiteValidFormat() {
+        assertThrows(InvalidCPFException.class, () -> CPF.getInstance("116.008.450-52"),
+                "Expected InvalidCPFException for an invalid CPF.");
     }
 
     @Test
-    public void isValidCPFFormat() throws InvalidCPFException {
+    public void shouldCreateInstanceForValidCPF() throws InvalidCPFException {
         CPF instance = CPF.getInstance("529.982.247-25");
-        assertNotNull(instance);
+        assertNotNull(instance, "Expected CPF instance to be created for a valid CPF.");
     }
-
-    private Optional<InvalidCPFException> catchInvalidCPFException(String cpf) {
-        InvalidCPFException exception;
-        exception = assertThrows(InvalidCPFException.class,
-                ()->{CPF.getInstance(cpf);}
-        );
-        return Optional.ofNullable(exception);
-    }
-
 }
