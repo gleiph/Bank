7ca169d6144ab6111c1412324d70a500b923ad27
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-08-19 12:32:56 -0300

    Cria Senha, SenhaTest e InvalidPasswordException

--- src/main/java/br/ufjf/dcc/model/utils/Senha.java
+++ src/main/java/br/ufjf/dcc/model/utils/Senha.java
@@ -1,4 +1,23 @@
 package br.ufjf.dcc.model.utils;
 
+import br.ufjf.dcc.model.exception.InvalidPasswordException;
+import lombok.Getter;
+
+@Getter
 public class Senha {
+
+    private String senha;
+
+    public static Senha getInstance(String senha) {return new Senha(senha);}
+
+    public Senha(String senha){
+        if(!isValid(senha)){
+            throw new InvalidPasswordException("Invalid password: " + senha);
+        }
+        this.senha = senha;
+    }
+
+    private boolean isValid(String senha){
+        return senha.length() >= 8;
+    }
 }
--- src/test/java/br/ufjf/dcc/model/SenhaTest.java
+++ src/test/java/br/ufjf/dcc/model/SenhaTest.java
@@ -1,4 +1,24 @@
 package br.ufjf.dcc.model;
 
+import br.ufjf.dcc.model.exception.InvalidPasswordException;
+import br.ufjf.dcc.model.utils.Senha;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SenhaTest {
+
+    @Test
+    public void shouldThrowExceptionForInvalidFormat() {
+        assertThrows(InvalidPasswordException.class, () ->
+                        Senha.getInstance("senha"),
+                "Expected InvalidPasswordException for incorrectly formatted password.");
+    }
+
+    @Test
+    public void shouldCreateInstanceForValidPassword() throws InvalidPasswordException {
+        Senha instance = Senha.getInstance("senha123");
+        assertNotNull(instance, "Expected Senha instance to be created for a valid password.");
+    }
 }
