282a4ce1359276c9ab144b00d8a091742b8a8760
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-08-19 12:38:07 -0300

    Cria entidade Conta

--- src/main/java/br/ufjf/dcc/model/Conta.java
+++ src/main/java/br/ufjf/dcc/model/Conta.java
@@ -1,4 +1,24 @@
 package br.ufjf.dcc.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
 public class Conta {
+
+    private final UUID id;
+    private final Cliente cliente;
+    @Setter
+    private double saldo;
+
+    public Conta(UUID id, Cliente cliente, double saldo){
+        this.id = id;
+        this.cliente = cliente;
+        this.saldo = saldo;
+    }
+
+    public static Conta getInstance(UUID id, Cliente cliente, double saldo) {return new Conta(id, cliente, saldo);}
+
 }
