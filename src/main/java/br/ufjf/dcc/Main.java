fe78033ba57f4d383c6b4186f58b26bc2ec9bd5a
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-09-10 17:39:37 -0300

    Cria teste de execução na main

--- src/main/java/br/ufjf/dcc/Main.java
+++ src/main/java/br/ufjf/dcc/Main.java
@@ -1,9 +1,15 @@
 package br.ufjf.dcc;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.ufjf.dcc.view.AdminView;
+import br.ufjf.dcc.view.LoginView;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        //LoginView view = new LoginView();
+        AdminView view = new AdminView();
+
+        view.desenha();
     }
 }
