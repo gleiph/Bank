3c17b98bf0ca737ca01ec0e7f3e19795dfb59992
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-09-14 12:00:09 -0300

    Cria ContaController e lógica de saque

---
+++ src/main/java/br/ufjf/dcc/controller/ContaController.java
@@ -0,0 +1,48 @@
+package br.ufjf.dcc.controller;
+
+import br.ufjf.dcc.model.Conta;
+import br.ufjf.dcc.model.Saque;
+import br.ufjf.dcc.model.exception.InvalidValueException;
+import br.ufjf.dcc.model.repository.ContaRepository;
+import br.ufjf.dcc.view.ClienteView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class ContaController {
+
+    public static class RealizaSaque implements ActionListener {
+
+        private final ClienteView tela;
+
+        public RealizaSaque(ClienteView tela) {
+            this.tela = tela;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                Conta contaCliente = tela.getContaCliente();
+                double valorSaque = Double.parseDouble(tela.getTfValorSaque().getText());
+
+                ContaRepository repository = new ContaRepository();
+                List<Conta> contas = repository.findAll();
+                int index = contas.indexOf(contaCliente);
+
+                Saque novoSaque = Saque.getInstance(contaCliente, valorSaque);
+                novoSaque.realiza();
+                contaCliente.getSaques().add(novoSaque);
+
+                contas.set(index, contaCliente);
+                repository.save(contas);
+
+                System.out.println("Successful withdraw");
+                tela.showSucessMessage("Saque realizado com sucesso.");
+            }
+            catch(InvalidValueException error){
+                tela.showErrorMessage(error.getMessage());
+            }
+        }
+    }
+}
