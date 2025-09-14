0244786128b26af21ac4ba63f3e5774f267488dc
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-09-14 12:01:10 -0300

    Cria fluxo de login na main

--- src/main/java/br/ufjf/dcc/Main.java
+++ src/main/java/br/ufjf/dcc/Main.java
@@ -1,15 +1,12 @@
 package br.ufjf.dcc;
 
-import br.ufjf.dcc.view.AdminView;
 import br.ufjf.dcc.view.LoginView;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //LoginView view = new LoginView();
-        AdminView view = new AdminView();
-
-        view.desenha();
+        LoginView view = new LoginView();
+        view.draw();
     }
 }
