397ff0eb6dd5c6ecf86612a4a73877f2dbd8338c
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-09-14 12:02:55 -0300

    Cria arrays de saques, depositos e transferencias em Conta

--- src/main/java/br/ufjf/dcc/model/Conta.java
+++ src/main/java/br/ufjf/dcc/model/Conta.java
@@ -1,24 +1,31 @@
 package br.ufjf.dcc.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
 public class Conta {
 
     private final UUID id;
-    @JsonIgnore
     private final Usuario cliente;
     @Setter
     private double saldo;
+    private List<Saque> saques;
+    private List<Deposito> depositos;
+    private List<Transferencia> transferencias;
 
-    private Conta(Usuario cliente, double saldo){
+    private Conta(Usuario cliente){
         this.id = UUID.randomUUID();
         this.cliente = cliente;
-        this.saldo = saldo;
+        this.saldo = 0;
+        this.saques = new ArrayList<>();
+        this.depositos = new ArrayList<>();
+        this.transferencias = new ArrayList<>();
     }
 
     private Conta() {
@@ -28,5 +35,19 @@ public class Conta {
 
     public static Conta createEmpty() {return new Conta();}
 
-    public static Conta getInstance(UUID id, Usuario cliente, double saldo) {return new Conta(cliente, saldo);}
+    public static Conta getInstance(Usuario cliente) {return new Conta(cliente);}
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return  Objects.equals(getId(), conta.getId()) &&
+                Objects.equals(getCliente(), conta.getCliente()) &&
+                Objects.equals(getSaldo(), conta.getSaldo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
